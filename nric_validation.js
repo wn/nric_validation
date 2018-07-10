a2f6ab2f5183a9a371966e5f824d69af4cbff441
Ang Wei Neng <weineng.a@gmail.com>
2018-07-10 22:57:50 +0800

    Fix minor bugs

--- nric_validation.js
+++ nric_validation.js
@@ -1,25 +1,28 @@
 function is_nric_valid(nric) {
-  if (
-    nric.length !== 9 ||
-    !['S', 'T', 'G', 'F'].includes(nric[0].toUpperCase())
-  ) {
-    return false;
-  }
-  var lastLetter = nric[8].toUpperCase();
-  var nricCheckDigits = '0ABCDEFGHIZJ';
-  var finCheckDigits = '0KLMNPQRTUWX';
-  var digits = nric.slice(1, 8);
-  var weights = [2, 7, 6, 5, 4, 3, 2];
-  var sum = 0;
+        if (
+          nric.length !== 9 ||
+          !['S', 'T', 'G', 'F'].includes(nric[0].toUpperCase())
+        ) {
+          return false;
+        }
+        var lastLetter = nric[8].toUpperCase();
+        var nricCheckDigits = 'JZIHGFEDCBA';
+        var finCheckDigits = 'XWUTRQPNMLK';
+        var digits = nric.slice(1, 8);
+        var weights = [2, 7, 6, 5, 4, 3, 2];
+        var sum = 0;
 
-  if (isNaN(digits)) {
-    return false;
-  }
-  for (var i = 0; i < digits.length; i++) {
-    sum += parseInt(digits.charAt(i), 10) * weights[i];
-  }
-  if (['S', 'F'].includes(nric[0].toUpperCase())) {
-    return nricCheckDigits.charAt(11 - (sum % 11)) === lastLetter;
-  }
-  return finCheckDigits.charAt(11 - ((sum + 4) % 11)) === lastLetter;
-}
+        if (isNaN(digits)) {
+          return false;
+        }
+        if (['T', 'G'].includes(nric[0].toUpperCase())) {
+          sum += 4;
+        }
+        for (var i = 0; i < digits.length; i++) {
+          sum += parseInt(digits.charAt(i), 10) * weights[i];
+        }
+        if (['S', 'T'].includes(nric[0].toUpperCase())) {
+          return nricCheckDigits.charAt(sum % 11) === lastLetter;
+        }
+        return finCheckDigits.charAt(sum % 11) === lastLetter;
+      }
