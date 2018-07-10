4450e456d18ad4dad944cba46e098c71a3d69cad
Ang Wei Neng <weineng.a@gmail.com>
2018-07-10 22:18:21 +0800

    First commit

---
+++ nric_validation.js
@@ -0,0 +1,26 @@
+function is_nric_valid(nric) {
+  if (
+    nric.length !== 9 ||
+    !['S', 'T', 'G', 'F'].includes(nric[0].toUpperCase())
+  ) {
+    return false;
+  }
+  var lastLetter = nric[8].toUpperCase();
+  var nricCheckDigits = '0ABCDEFGHIZJ';
+  var finCheckDigits = '0KLMNPQRTUWX';
+  var digits = nric.slice(1, 8);
+  var weights = [2, 7, 6, 5, 4, 3, 2];
+  var sum = 0;
+
+  // eslint-disable-next-line no-restricted-globals
+  if (isNaN(digits)) {
+    return false;
+  }
+  for (var i = 0; i < digits.length; i++) {
+    sum += parseInt(digits.charAt(i), 10) * weights[i];
+  }
+  if (['S', 'F'].includes(nric[0].toUpperCase())) {
+    return nricCheckDigits.charAt(11 - (sum % 11)) === lastLetter;
+  }
+  return finCheckDigits.charAt(11 - ((sum + 4) % 11)) === lastLetter;
+}
